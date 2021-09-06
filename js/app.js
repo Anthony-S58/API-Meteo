17c7ae34cf12c75a65c004a9f62d753ace0388a2
Anthony-S58 <tonysim01@hotmail.com>
2021-09-06 14:56:24 +0200

    addStyle

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 // on insère la key API
 const APIKEY = '' ;
 // on récupère l'url dans la documentation du site 
-let url = `https://api.openweathermap.org/data/2.5/weather?q=Nevers&appid=${APIKEY}`;
+// units=metric correspond à la récupération de la température en celsius
 
-fetch(url).then( (response) => response.json().then((data) => console.log(data))
-);
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Nevers&units=metric&lang=fr&appid=${APIKEY}`;
+
+fetch(url).then( (response) => response.json().then((data) => 
+    {console.log(data);
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.temperature').innerHTML = "<i class='fas fa-thermometer-half'></i>" + data.main.temp + '°';
+        document.querySelector('.humidity').innerHTML = "<i class='fas fa-tint'></i>" + data.main.humidity + '%';
+        document.querySelector('.wind').innerHTML = "<i class='fas fa-wind'></i>" + data.wind.speed + 'km/h';
+    })
+).catch(err => console.log('Erreur : ' + err));
